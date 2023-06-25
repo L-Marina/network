13024b8e3a8ebece4cef5ed3b9ed609d8c40cbe1
L-Marina <marina.liemak@gmail.com>
2023-06-25 23:18:17 +0300

    fix

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import classes from './Button.module.css';
 
-export const Button = ({children, ...props}) => {
+export const Button = ({ children, ...props }) => {
 	return (
 		<button {...props} className={classes.btn}>
 			{children}
